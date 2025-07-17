8394549833f370bd6c1bbc251988efb5bcdba18f
Mohammad Rahimzadeh <mohammad1079pro@gmail.com>
2025-07-17 08:31:59 +0330

    Add tests for todo urls

--- todo/tests/test_todo_urls.py
+++ todo/tests/test_todo_urls.py
@@ -1,14 +1,14 @@
-# from django.test import TestCase
-# from django.urls import reverse, resolve
+from django.test import TestCase
+from django.urls import reverse, resolve
 
-# from ..views import ListItemView, SingleItemView
+from ..views import ListItemView, SingleItemView
 
 
-# class TestUrls(TestCase):
-#     def test_todo_item_list_urls_resolve(self):
-#         url = reverse("todo:list-item")
-#         self.assertEqual(resolve(url).func.view_class, ListItemView)
+class TestTodoUrls(TestCase):
+    def test_todo_item_list_urls_resolve(self):
+        url = reverse("todo:list-item")
+        self.assertEqual(resolve(url).func.view_class, ListItemView)
 
-#     def test_todo_item_single_urls_resolve(self):
-#         url = reverse("todo:single-item", kwargs={'pk':1})
-#         self.assertEqual(resolve(url).func.view_class, SingleItemView)
+    def test_todo_item_single_urls_resolve(self):
+        url = reverse("todo:single-item", kwargs={'pk':1})
+        self.assertEqual(resolve(url).func.view_class, SingleItemView)
